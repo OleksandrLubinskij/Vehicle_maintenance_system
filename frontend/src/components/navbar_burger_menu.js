eddb90666c8829a5948e3e28f069e1414d60bbd0
Darkentrol <oleksandrlubinskij@gmail.com>
2026-06-24 15:20:43 +0300

    Adaptibility done

--- frontend/src/components/navbar_burger_menu.js
+++ frontend/src/components/navbar_burger_menu.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const menuToggle = document.getElementById("menu-toggle");
-  const navMenu = document.getElementById("nav-menu");
-
+  const menuToggle = document.querySelector("#menu_toggle");
+  const navMenu = document.querySelector("#mobile_dropdown_menu");
+  const menu_elem_btns = document.querySelectorAll(".menu_btn");
+  console.log(navMenu)
   if (menuToggle && navMenu) {
     menuToggle.addEventListener("click", () => {
+      console.log("Hello")
       navMenu.classList.toggle("hidden");
       navMenu.classList.toggle("flex");
     });
   }
+
+  menu_elem_btns.forEach(menu_btn => {
+    menu_btn.addEventListener("click", () => {
+      navMenu.classList.toggle("hidden");
+      navMenu.classList.toggle("flex");
+    })
+  })
 });
