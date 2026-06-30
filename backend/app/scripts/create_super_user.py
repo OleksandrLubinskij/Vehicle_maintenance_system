22599d468765bbfe5c2b1faf9ca3d39e25112bf8
Darkentrol <oleksandrlubinskij@gmail.com>
2026-06-30 15:48:50 +0300

    Add info to create super user func output

--- backend/app/scripts/create_super_user.py
+++ backend/app/scripts/create_super_user.py
@@ -5,18 +5,29 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+GREEN = "\033[92m"
+RED = "\033[91m"
+CYAN = "\033[96m"
+RESET = "\033[0m"
+
 async def create_super_user():
+    username = "admin"
+    raw_password = "secure_password"
+    
     async with SessionLocal() as session:
-        hased_password = pwd_context.hash("secure_password")
+        hased_password = pwd_context.hash(raw_password)
         super_user = User(
-            login="admin",
+            login=username,
             password=hased_password,
             role="Admin"
         )
         session.add(super_user)
         await session.commit()
     
-    print("Super user created successfully.")
+    print(f"\n{GREEN}Суперкористувача успішно створено!{RESET}")
+    print(f"Логін: {CYAN}{username}{RESET}")
+    print(f"Пароль: {CYAN}{raw_password}{RESET}")
+    print(f"{RED}УВАГА:Обов'язково змініть цей пароль після першого входу в систему!{RESET}\n")
 
 if __name__ == "__main__":
     asyncio.run(create_super_user())
