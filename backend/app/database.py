436f75871966a2fd95b2bf7a692982248c9fdc90
Darkentrol <oleksandrlubinskij@gmail.com>
2026-07-01 12:26:35 +0300

    bug fix, forget pass connec_args param

--- backend/app/database.py
+++ backend/app/database.py
@@ -10,7 +10,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 connect_args = {}
 if "pooler.supabase.com" in DATABASE_URL:
     connect_args = {"server_settings": {"statement_cache_size": "0"}}
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL,connect_args=connect_args)
 
 SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
